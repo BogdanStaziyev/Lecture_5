cfe9721d148f691a1d3c43fb52d247064a4d9db1
BogdanStaziyev <94914224+BogdanStaziyev@users.noreply.github.com>
2022-06-21 22:40:08 +0300

    "TASK_2 fix"

--- task_2/main.go
+++ task_2/main.go
@@ -7,11 +7,15 @@ import (
 )
 
 func main() {
-	input := "1 9 3 4 -5"
+	input := "1 2 3 4 5"
 	var result string
 	var min, max int32
 
 	inputSlip := strings.Split(input, " ")
+
+	m, _ := strconv.Atoi(inputSlip[0])
+	min = int32(m)
+
 	for _, val := range inputSlip {
 		i, _ := strconv.Atoi(val)
 		if min >= int32(i) {
