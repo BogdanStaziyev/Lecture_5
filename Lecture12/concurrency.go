f9c7afb8a07e45fbab750cfe8279db0276b3b6c1
BogdanStaziyev <94914224+BogdanStaziyev@users.noreply.github.com>
2022-07-19 11:37:04 +0300

    "concurrency"

---
+++ Lecture12/concurrency.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var count int
+
+func main() {
+	var wg sync.WaitGroup
+
+	n := [][]int{
+		{2, 6, 9, 24},
+		{7, 3, 94, 3, 0},
+		{4, 2, 8, 35},
+	}
+
+	wg.Add(len(n))
+	for _, val := range n {
+		value := val
+		go func() {
+			sum(value)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
+
+func sum(sls []int) {
+	var sumOfSlice int
+	count++
+
+	for _, num := range sls {
+		sumOfSlice += num
+	}
+	fmt.Printf("slise %d: %d\n", count, sumOfSlice)
+}
